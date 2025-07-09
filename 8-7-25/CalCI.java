02e84a6e950ea5db1f24a615a0ebbe24d10d4ca9
Prajwal Bharambe <131812712+Pdeweloper@users.noreply.github.com>
2025-07-09 13:23:18 +0530

    Update CalCI.java

--- 8-7-25/CalCI.java
+++ 8-7-25/CalCI.java
@@ -5,17 +5,20 @@ public static void main(String args[])
 Object rt=5,T=3;
 float P = 1000.0F,n=1;
 
-double rt1 = (int)rt/100;
-int T1 = (int)T*(int)n;
+int R = (int)rt;
+int t = (int)T;
 
-System.out.print(T1+" "+rt1);
+float R1 = R/100f;
 
-double A = P*(Math.pow((1+(rt1/n)),T1));
+double a = Math.pow((1+(R1/n)),n*t);
+double A = P*a;
+
+double CI = A-P;
 
 //double A = 1000*(Math.pow((1+(0.05/1)),3));
 //double CI = A-P;
 
-//System.out.print("Compound Intrest is : "+A);
+System.out.print("Compound Intrest is : "+CI);
 
 }
 }
